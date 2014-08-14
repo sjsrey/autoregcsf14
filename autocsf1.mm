1aa1994931044b937f1e64e11cba427196d6f34a
Robert Pahle <rpahle@gmx.de>
2014-08-14 17:10:50 -0400

    new files from rob

---
+++ autocsf1.mm
@@ -0,0 +1,219 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1283093380553" ID="ID_1723255651" MODIFIED="1408049064963" TEXT="Parallel PySAL">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<hook NAME="MapStyle">
+<properties SHOW_ICON_FOR_ATTRIBUTES="true" SHOW_NOTES_IN_MAP="true" SHOW_NOTE_ICONS="true"/>
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="default" MAX_WIDTH="600" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge/>
+<cloud/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode COLOR="#18898b" LOCALIZED_TEXT="styles.topic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#cc3300" LOCALIZED_TEXT="styles.subtopic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#669900" LOCALIZED_TEXT="styles.subsubtopic">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="AutomaticLayout.level.root">
+<font/>
+</stylenode>
+<stylenode COLOR="#0033ff" LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font/>
+</stylenode>
+<stylenode COLOR="#00b439" LOCALIZED_TEXT="AutomaticLayout.level,2">
+<font/>
+</stylenode>
+<stylenode COLOR="#990000" LOCALIZED_TEXT="AutomaticLayout.level,3">
+<font/>
+</stylenode>
+<stylenode COLOR="#111111" LOCALIZED_TEXT="AutomaticLayout.level,4">
+<font/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<attribute_layout NAME_WIDTH="71" VALUE_WIDTH="408"/>
+<attribute NAME="subtitle" VALUE="Autoregression and Complex System Framework Integration"/>
+<attribute NAME="theme" VALUE="Montpellier"/>
+<attribute NAME="author" VALUE="Jason Laura, Robert Pahle, Sergio Rey, Luc Anselin"/>
+<attribute NAME="institute" VALUE="GeoDa Center for Geospatial Analysis and Computation\\Arizona State University"/>
+<attribute NAME="date" VALUE="\today"/>
+<attribute NAME="usepackage" VALUE="verbatim"/>
+<node COLOR="#0033ff" CREATED="1407791047001" ID="ID_1032477183" MODIFIED="1408049064974" POSITION="right" TEXT="PySAL">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1407811785353" ID="ID_609223159" MODIFIED="1408049064978" TEXT="PySAL">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1407791354995" ID="ID_324044368" MODIFIED="1408049064979" TEXT="PySAL">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="leftcolumnwidth" VALUE="0.4"/>
+<node COLOR="#111111" CREATED="1407792203177" ID="ID_1939634079" MODIFIED="1408049064982">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="pysalGraphic.png"/>
+  </body>
+</html></richcontent>
+<attribute NAME="height" VALUE="0.7\textheight"/>
+</node>
+<node COLOR="#111111" CREATED="1407794578001" ID="ID_244477005" MODIFIED="1408049065061" TEXT="Spatial analysis library"/>
+<node COLOR="#111111" CREATED="1407792357978" ID="ID_373869380" MODIFIED="1408049065061" TEXT="Big data world"/>
+<node COLOR="#111111" CREATED="1407794398828" ID="ID_784146886" MODIFIED="1408049065061" TEXT="v 1.8 July 2014"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1407791072914" ID="ID_605225448" MODIFIED="1408049065065" TEXT="Parallel PySAL">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1407791354995" ID="ID_1994431929" MODIFIED="1408049065066" TEXT="pPySAL">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="leftcolumnwidth" VALUE="0.3"/>
+<node COLOR="#111111" CREATED="1407792203177" ID="ID_1121605984" MODIFIED="1408049065066">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="pysalGraphic.png"/>
+  </body>
+</html></richcontent>
+<attribute NAME="height" VALUE="0.7\textheight"/>
+</node>
+<node COLOR="#111111" CREATED="1407794578001" ID="ID_1205917437" MODIFIED="1408049065145" TEXT="contiguity builder"/>
+<node COLOR="#111111" CREATED="1407794398828" ID="ID_1705569275" MODIFIED="1408049065145" TEXT="max-p region"/>
+<node COLOR="#111111" CREATED="1407792357978" ID="ID_1269693491" MODIFIED="1408049065146" TEXT="p-lisa"/>
+<node COLOR="#111111" CREATED="1407809748259" ID="ID_636514524" MODIFIED="1408049065146" TEXT="fisher jenks"/>
+<node COLOR="#111111" CREATED="1407809737573" ID="ID_1592508043" MODIFIED="1408049065146" TEXT="spatial regimes"/>
+</node>
+<node COLOR="#990000" CREATED="1407793929808" ID="ID_1278363927" MODIFIED="1408049065147" TEXT="Lessons Learned">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1407793946292" ID="ID_1214235800" MODIFIED="1408049065151" TEXT="Hardware dependence"/>
+<node COLOR="#111111" CREATED="1407793936453" ID="ID_955758284" MODIFIED="1408049065152" TEXT="No holy grail of automatic parallelization"/>
+<node COLOR="#111111" CREATED="1407793992138" ID="ID_1823286665" MODIFIED="1408049065152" TEXT="Need a roadmap = Taxonomy">
+<node COLOR="#111111" CREATED="1407810519990" ID="ID_292241150" MODIFIED="1408049065152" TEXT="Guidance on &quot;best practice&quot;"/>
+<node COLOR="#111111" CREATED="1407810587387" ID="ID_325625207" MODIFIED="1408049065153" TEXT="Identify dead ends"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1407791081812" ID="ID_1236523920" MODIFIED="1408049065162" POSITION="right" TEXT="Substantive Application: Spatial Econometrics">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1407791090816" ID="ID_1025584080" MODIFIED="1408049065168" TEXT="Specification Strategies">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1407792637767" ID="ID_652988510" MODIFIED="1408049065168" TEXT="Spatial Econometrics">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1407792710082" ID="ID_603384886" MODIFIED="1408049065171">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="geodaspace.png"/>
+  </body>
+</html></richcontent>
+<attribute NAME="height" VALUE="0.7\textheight"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1407792939666" ID="ID_781173576" MODIFIED="1408049065188" TEXT="GeoDaSpace">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1407792994096" ID="ID_1741410635" MODIFIED="1408049065190" TEXT="GUI ontop of spreg">
+<hook NAME="ExternalObject"/>
+<attribute NAME="height" VALUE="0.7\textheight"/>
+</node>
+<node COLOR="#111111" CREATED="1407793064660" ID="ID_22710692" MODIFIED="1408049065190" TEXT="Subset of spreg functionality"/>
+<node COLOR="#111111" CREATED="1407793129085" ID="ID_1237290818" MODIFIED="1408049065190" TEXT="Cross-platform"/>
+</node>
+<node COLOR="#990000" CREATED="1407792635790" ID="ID_1086235569" MODIFIED="1408049065191" TEXT="Specification Searches">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1407792662459" ID="ID_470997398" MODIFIED="1408049065192" TEXT="Specific to General">
+<node COLOR="#111111" CREATED="1407810760693" ID="ID_124769570" MODIFIED="1408049065192" TEXT="$y=X\beta + \epsilon$"/>
+<node COLOR="#111111" CREATED="1407811216411" ID="ID_1223518957" MODIFIED="1408049065193" TEXT="OLS + Lagrange Multiplier Tests"/>
+</node>
+<node COLOR="#111111" CREATED="1407792665732" ID="ID_15698420" MODIFIED="1408049065193" TEXT="General to Specific">
+<node COLOR="#111111" CREATED="1407810785369" ID="ID_1519058222" MODIFIED="1408049065193" TEXT="$y = \rho W y + X\beta + (I-\lambda W)^{-1} \nu$"/>
+<node COLOR="#111111" CREATED="1407811240103" ID="ID_609953892" MODIFIED="1408049065194" TEXT="ML + Restrictions"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1407793530699" ID="ID_1454365887" MODIFIED="1408049065194" TEXT="LM Based Specification">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1407793538476" ID="ID_1757889662" MODIFIED="1408049065195">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="decision3.pdf"/>
+  </body>
+</html></richcontent>
+<attribute NAME="height" VALUE="0.9 \textheight"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1407791111461" ID="ID_66876990" MODIFIED="1408049065201" TEXT="ArcGIS Toolbox">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1407793381236" ID="ID_1298066915" MODIFIED="1408049065202" TEXT="ArcGIS Toolbox">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1407793400019" ID="ID_1621353770" MODIFIED="1408049065204" TEXT=""/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1407791119599" ID="ID_208524455" MODIFIED="1408049065205" POSITION="right" TEXT="Implementation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1407791125024" ID="ID_273895851" MODIFIED="1408049065207" TEXT="Parallelization">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1407806831967" ID="ID_537121155" MODIFIED="1408049094847">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="setup1.png" />
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1407806848793" ID="ID_1730368076" MODIFIED="1408049065209" TEXT="Parallel Strategy">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1407791132284" ID="ID_310127908" MODIFIED="1408049065211" TEXT="CSF">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1407806867273" ID="ID_1925462953" MODIFIED="1408049065212" TEXT="Complex Systems Framework">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1407806884295" ID="ID_1837931657" MODIFIED="1408049065214" TEXT="Components for Autoreg">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
